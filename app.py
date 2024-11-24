f390c3c5a7739e158667e078ef05e2a48e0a64b2
Joss2309 <psarias23@gmail.com>
2024-11-24 16:35:07 -0600

    Mi Blog

--- app.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-import plotly.express as px
-
-
-st.write("Mi Blog")
-st.markdown('''
-            :heart:
-            ''')
---
+++ star.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import plotly.express as px
+
+
+st.write("My first Blog")
+
+st.markdown('''
+            :tulip:
+            :heart_eyes:
+            :sparkles:
+            :tulip:
+
+            # Datos Sakila
+            ''')
